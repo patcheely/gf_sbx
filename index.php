188f70e08f5e03e3f12425a788dc50633874a954
Patrick Cheely <pat.cheely@gmail.com>
2015-07-30 08:40:36 -0400

    index.php created
    Created an index page for sandbox

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang='cs'>
+  <head>
+    <title></title>
+    <meta charset='utf-8'>
+    <meta name='description' content='sandbox.gadgetfool.com Index'>
+    <meta name='author' content='Patrick Cheely'>
+    <meta name='robots' content='noindex, nofollow'>
+  </head>
+  <body>
+    <h1>Hello World</h1>
+  </body>
+</html>
