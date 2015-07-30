306cbe3d0257802a919292b089820b9936db221a
Patrick Cheely <pat.cheely@gmail.com>
2015-07-30 08:51:29 -0400

    Added author variable
    Added author variable and PHP tags.

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
+<?php
+  $author = "Patrick Cheely";
+?>
 <!DOCTYPE html>
 <html lang='cs'>
   <head>
     <title></title>
     <meta charset='utf-8'>
     <meta name='description' content='sandbox.gadgetfool.com Index'>
-    <meta name='author' content='Patrick Cheely'>
+    <meta name='author' content='<?php echo $author;?>'>
     <meta name='robots' content='noindex, nofollow'>
   </head>
   <body>
